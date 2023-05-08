efb859b2f996c6d630ef30faa47cc1367c2a5dff
Subhranil0509 <120248380+Subhranil0509@users.noreply.github.com>
2023-05-08 22:05:21 +0530

    Find a number If it's even or odd

---
+++ OddEven.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("Enter a number");
+scanf("%d",&n);
+if(n % 2 == 0 )
+printf("The number %d is Even",n);
+else
+printf("The number %d is Odd",n);
+return 0;
+}
