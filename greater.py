5e4e1e9dc84402fd6ca43566c0c90d69a2403785
Subhranil Das <120248380+Subhranil0509@users.noreply.github.com>
2023-05-28 16:07:54 +0530

    WAP to check greater in python

---
+++ greater.py
@@ -0,0 +1,6 @@
+a = int(input("Enter the 1st Number: "))
+b = int(input("Enter the 2nd Number: "))
+if a>b:
+    print("a =",a,"is greater than b =",b)
+else:
+    print("b =",b,"is greater than a =",a)
